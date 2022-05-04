96c6d45ac9fc5d633e765ce5cdd2070386741767
TankistPro <nakol.nayk@mail.ru>
2022-05-04 19:46:29 +0300

    fix db ssl

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DB_DATABASE, 
-  process.env.DB_USERNAME, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
     dialect: process.env.DB_DIALECT,
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+      // ssl: {
+      //   require: true,
+      //   rejectUnauthorized: false
+      // }
     },
     host: process.env.DB_HOST,
     PORT: process.env.DB_PORT
