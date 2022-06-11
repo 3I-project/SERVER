4226cbe875c3fa4b14b8e3cb15ab61b3e1382c8d
TankistPro <nakol.nayk@mail.ru>
2022-06-11 16:23:39 +0300

    f test

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,10 +3,19 @@ const fs = require('fs');
 const apiVersoin = '/apiV1';
 
 module.exports.initRoutes = (app) => {
-  fs.readdirSync(__dirname).forEach(f => {
+  /*fs.readdirSync(__dirname).forEach(f => {
     if(f !== 'main.js') {
       const filename = f.substring(0, f.indexOf('.'))
       require('./' + f).init(app, apiVersoin);
     }
-  })
+  })*/
+
+  require('./auth.route').init(app, apiVersoin);
+  require('./comment.route').init(app, apiVersoin);
+  require('./idea.route').init(app, apiVersoin);
+  require('./ideaReactions.route').init(app, apiVersoin);
+  require('./image.route').init(app, apiVersoin);
+  require('./organization.route').init(app, apiVersoin);
+  require('./token.route').init(app, apiVersoin);
 }
+
