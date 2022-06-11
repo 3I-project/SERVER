5b6a0a0fcab79bb467d4c42a4d89f9e8dc4d08a8
TankistPro <nakol.nayk@mail.ru>
2022-06-11 14:58:51 +0300

    fix

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,15 +3,9 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD,	
   {
-    dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-      // ssl: {
-      //   require: true,
-      //   rejectUnauthorized: false
-      // }
-    },
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     PORT: process.env.DB_PORT
   }
